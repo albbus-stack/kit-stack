af4eed784fd36386a5eb4ddb93a09785af54250f
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-07-21 20:34:18 +0200

    Added zod and user memorization

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { PrismaClient } from '@prisma/client';
 
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
 	const code = url.searchParams.get('code');
@@ -7,6 +8,17 @@ export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
+
+	const user = await supabase.auth.getUser();
+
+	if (!user.data?.user?.email) throw redirect(303, '/');
+
+	await new PrismaClient().user.create({
+		data: {
+			email: user.data?.user?.email,
+			id: user.data?.user?.id
+		}
+	});
 
 	throw redirect(303, '/');
 };
