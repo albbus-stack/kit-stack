8331e56ec5dcb21b33aa7d45f7fd28b43d312285
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-11-07 18:35:29 +0000

    Fixed LanguageSwitch margin

--- src/lib/components/LanguageSwitch.svelte
+++ src/lib/components/LanguageSwitch.svelte
@@ -11,7 +11,7 @@
         <img class="h-8 w-8" src={`https://hatscripts.github.io/circle-flags/flags/${lang === "en" ? "us" : lang}.svg`} alt={lang}/>
     </button>
     <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-    <ul tabindex="0" class="dropdown-content z-[1000] p-2 menu shadow bg-base-200 rounded-box">
+    <ul tabindex="0" class="dropdown-content z-[1000] p-2 menu shadow bg-base-200 rounded-box gap-1">
         {#each availableLanguageTags as languageTag}
             {#if languageTag !== lang}
                 <li>
@@ -19,7 +19,7 @@
                         on:click={() => {
                             setLanguageTag(languageTag)
                         }} 
-                        class="p-2 py-1 mb-1"
+                        class="p-2 py-1"
                     >
                         <img class="h-10 w-10" src={`https://hatscripts.github.io/circle-flags/flags/${languageTag === "en" ? "us" : languageTag}.svg`} alt={languageTag}/>
                     </button>
