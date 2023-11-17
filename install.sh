f503f2f70dba6f7243f695b7264729daef49c011
Alberto <alberto.delbuonopaolini@gmail.com>
2023-11-17 11:37:14 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git clone https://github.com/albbus-stack/kit-stack.git $folderName
 cd $folderName
 rm -rf .git LICENSE install.sh install.ps1
 mv .env.local.example .env.local
-pnpm install
-pnpm db:generate
-pnpm env:sync
+bun install
+bun db:generate
+bun env:sync
 git init
